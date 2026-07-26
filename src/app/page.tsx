433d0c753184300d9418e8258819e7c20cd9e4c8
koushiksamui <samuikousik95@gmail.com>
2026-07-27 00:07:56 +0530

    feat: redirect root path to notebooks page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from 'next/navigation';
 
-
-export default async function Page() {
-  return (
-    <div>Hello World</div>
-  )
-}
+export default function Page() {
+  redirect('/notebooks');
+}
